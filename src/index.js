3bdce2560f488092f592efed7b4d199583f8b303
Nischay <nischayrawat100@gmail.com>
2024-07-22 16:25:32 +0530

    Utils middleware addded

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import express from 'express'
 import connectDB from './db/index.js'
 
 const app = express();
-connectDB()
+connectDB().then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+}).catch(()=>{
+    console.log("Mongo db connection failed !!!",err)
+})
